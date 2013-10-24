f14171b941b0b9b7e7a63d15d01d19cdf67a2644
Peter West <pete@whiteoctober.co.uk>
2013-10-24 16:01:25 +0100

    Final fixes

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ $(function () {
         var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
         $(this).on('mugorientation', function(e) {
             var coffee, drink;
-            
+
             drink = __bind(function(coffee, angle) {
                 return Math.min($(this).height() * angle, coffee.height());
             }, this);
-            
+
             coffee = $('.coffee', this);
             if (!coffee.size()) {
                 return;
             }
             coffee.height(drink(coffee, e.angle));
-            
+
             if (coffee.height() <= 0) {
                 coffee.remove();
                 return $(this).trigger('more.coffee');
@@ -34,11 +34,11 @@ $(function () {
         });
         $(this).on('more.coffee', function() {
             return $.get('coffee://trojan', {
-            
+
                 success: __bind(function(coffee) {
                     return $(this).append(coffee);
                 }, this),
-                
+
                 statusCode: {
                     418: function() {
                         return alert("I'm a tea pot");
@@ -50,9 +50,9 @@ $(function () {
 
     var mug = $('#mug');
     mug.each(mugCode);
-    
+
     var oriented = navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i);
-    if (!oriented) $("header h1").text("Spin to drink coffee");    
+    if (!oriented) $("header h1").text("Spin to drink coffee");
 
     var mugAngle = 0;
     var curr = {x: 0, y: 0, angle: 0};
@@ -61,27 +61,27 @@ $(function () {
     var drawRotatedMug = function(mugEvent) {
         if (oriented) mug.rotate(-(1 - mugEvent.angle) * 90);
         mug.children(".front, .back").rotate((1 - mugEvent.angle) * 90);
-        
+
         if (mugEvent.angle > 0.5 && mugEvent.angle < 1.5) {
             var dir = mugEvent.angle > 1 ? 1 : -1;
             mug.children(".coffee").css("left", dir * (1 - mugEvent.angle) * 60);
             mug.find(".coffee img").css("width", 370 + Math.abs(1 - mugEvent.angle) * 300);
         }
-        
+
         mugEvent.angle = mugEvent.angle > 1 ? 1 - (mugEvent.angle - 1) : mugEvent.angle;
         mug.children(".background").css("top", 680 - mug.children(".coffee").height());
     }
-    
+
     var hasRotated = function(a, b) {
         return a.angle > b.angle && a.angle - b.angle < Math.PI/2 && a.angle - b.angle > Math.PI/32;
     }
-    
-    $(".button.about").click(function() { 
-        $(".popup").toggle(); 
-        return false; 
+
+    $(".button.about").click(function() {
+        $(".popup").toggle();
+        return false;
     });
     $(document).on('click', function() { $(".popup").hide(); });
-    
+
     $(document).on('mousemove', function(e) {
         var next = {x: e.clientX, y: e.clientY};
         next.angle = Math.atan2(curr.x - next.x, curr.y - next.y);
@@ -95,13 +95,13 @@ $(function () {
             if (direction-- < -buffer) mugAngle--;
         }
         curr = next;
-        
+
         var mugEvent = jQuery.Event("mugorientation");
         mugEvent.angle = 1 - mugAngle / 30;
         drawRotatedMug(mugEvent);
         mug.trigger(mugEvent);
     });
-    
+
     if (oriented) {
         var betafirst;
         // Mugs are slightly different from browsers
@@ -114,11 +114,11 @@ $(function () {
 
             drawRotatedMug(mugEvent);
             mug.trigger(mugEvent);
-        });  
+        });
 
         var orientStart = window.orientation
-        var reorient = function(e) { 
-            $("#content .wrapper").rotate(orientStart - window.orientation); 
+        var reorient = function(e) {
+            $("#content .wrapper").rotate(orientStart - window.orientation);
             var mugEvent = jQuery.Event("mugorientation");
             mugEvent.angle = 0;
             mug.trigger(mugEvent);
